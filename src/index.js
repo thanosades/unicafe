d63330c1282e7d2b01cec11fc3bb115123d3bedd
Thanos Dimitriades <78486373+thanosades@users.noreply.github.com>
2021-02-15 14:59:32 +0200

    solution for exercise 1.7

--- src/index.js
+++ src/index.js
@@ -5,13 +5,21 @@ const Header = ({ text }) => (<h1>{text}</h1>)
 
 const Button = ({ text, handleClick }) => (<button onClick={handleClick}>{text}</button>)
 
-const Stats = ({ good, neutral, bad }) => (
-  <div>
+const Stats = ({ good, neutral, bad }) => {
+  const totalVotes = good + neutral + bad
+  const totalPoints = good - bad
+  const average = totalPoints / totalVotes
+  const positive = good / totalVotes * 100
+
+  return (<div>
     <div>good {good}</div>
     <div>neutral {neutral}</div>
     <div>bad {bad}</div>
-  </div>
-)
+    <div>all {totalVotes}</div>
+    <div>average {average}</div>
+    <div>positive {positive} %</div>
+  </div>)
+}
 
 const App = () => {
   // save clicks of each button to its own state
@@ -22,9 +30,9 @@ const App = () => {
   return (
     <div>
       <Header text='give feedback' />
-      <Button text='good' handleClick={() => setGood(good + 1)}/>
-      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)}/>
-      <Button text='bad' handleClick={() => setBad(bad + 1)}/>
+      <Button text='good' handleClick={() => setGood(good + 1)} />
+      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
+      <Button text='bad' handleClick={() => setBad(bad + 1)} />
       <Header text='statistics' />
       <Stats good={good} neutral={neutral} bad={bad} />
     </div>
